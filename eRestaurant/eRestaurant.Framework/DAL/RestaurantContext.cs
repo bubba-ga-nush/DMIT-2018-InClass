0dcf8ed864fea9524eee74721ac4d4bfd61ddd0e
bubba-ga-nush <TPudar1@WA320-22.nait.ca>
2015-09-22 16:41:46 -0600

    Created DAL and BLL

---
+++ eRestaurant/eRestaurant.Framework/DAL/RestaurantContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity; // Needed for the DbContext and other EF classes
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eRestaurant.Framework.Entities; // Needed for entity classes
+
+namespace eRestaurant.Framework.DAL
+{
+    // By making our DAL class internal, that means outside project can't directly 
+    // access our Data Access Layer (they will have to go through our BLL to do stuff)
+    internal class RestaurantContext : DbContext
+    {
+        // One property for each Table/Entity in the database
+        public DbSet<MenuCategory> MenuCategories { get; set; }
+        public DbSet<Item> Items { get; set; }
+    }
+}
--- eRestaurant/eRestaurant.Framework/Entities/ReservationTable.cs
+++ eRestaurant/eRestaurant.Framework/Entities/ReservationTable.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace eRestaurant.Framework.Entities
 {
-    class ReservationTable
+    public class ReservationTable
     {
+        [Key]
+        public int TableID { get; set; }
+        public int ReservationID { get; set; }
+
+        // Navigation Property
+        public virtual Table Table { get; set; }
+        public virtual Reservation Reservation { get; set; }
     }
 }
